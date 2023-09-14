2304ebc0fb2db1aff190ac447b84685ebb82207d
Trevor Schlack <schlackt@gmail.com>
2023-09-14 16:42:57 -0400

    fix: read directly from tcp stream

--- src/main.rs
+++ src/main.rs
@@ -14,24 +14,21 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buf_reader = BufReader::new(&mut stream);
-    let mut buffer = String::new();
+    println!("Read message");
 
-    let request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .map(|message| message.into_bytes())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let mut buffer = [0u8; 15];
+    stream.read_exact(&mut buffer).unwrap();
 
-    for message in request {
-        let length = message[0] as u16 | ((message[1] as u16) << 8);
-        let message_type = message[2];
-        println!("--- message received ---");
-        dbg!(length);
-        dbg!(message_type);
-        println!("{}", String::from_utf8(message[3..].to_vec()).unwrap());
-    }
+    println!("Processing messages");
 
-    println!("Request: {:#?}", buffer.as_bytes());
+    let length = buffer[0] as u16 | ((buffer[1] as u16) << 8);
+    let message_type = buffer[2];
+
+    println!("--- message received ---");
+    dbg!(length);
+    dbg!(message_type);
+
+    println!("{}", String::from_utf8(buffer[3..].to_vec()).unwrap());
+
+    println!("Request: {:#?}", buffer);
 }
