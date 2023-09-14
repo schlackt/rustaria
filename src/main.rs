88f8eee9231e115e2c2f4dec132ef64f2c56e2ad
Trevor Schlack <schlackt@gmail.com>
2023-09-14 13:52:55 -0400

    feat: parse client -> server connection request

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{
 };
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7777").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
@@ -14,12 +14,24 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
+    let mut buf_reader = BufReader::new(&mut stream);
+    let mut buffer = String::new();
+
     let request: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap())
+        .map(|message| message.into_bytes())
         .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("Request: {:#?}", request);
+    for message in request {
+        let length = message[0] as u16 | ((message[1] as u16) << 8);
+        let message_type = message[2];
+        println!("--- message received ---");
+        dbg!(length);
+        dbg!(message_type);
+        println!("{}", String::from_utf8(message[3..].to_vec()).unwrap());
+    }
+
+    println!("Request: {:#?}", buffer.as_bytes());
 }
